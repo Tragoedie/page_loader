af3a55adaf231d8a89d094192dabc1d39a98e5c5
Tragoedia <tragoedia@mail.ru>
2022-04-15 02:06:06 +0700

    fix test

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,4 +1,4 @@
-from page_loader.download import download_html, download_local_files, download
+from page_loader.download import download_html, download
 from page_loader.get_name import get_folder_name, get_html_name, get_local_file_path
 import requests_mock
 import tempfile
